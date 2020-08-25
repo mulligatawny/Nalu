32c202904ad1b087d4648365675410e1f932479b
mulligatawny <mail.markbenjamin@gmail.com>
2020-08-25 16:48:56 -0700

    .

--- src/InputOutputRealm.C
+++ src/InputOutputRealm.C
@@ -66,7 +66,7 @@ InputOutputRealm::initialize()
   create_output_mesh();
   input_variables_from_mesh();
   initialize_post_processing_algorithms();
-  compute_wall_distance();
+  //compute_wall_distance();
 }
 
 //--------------------------------------------------------------------------
@@ -204,8 +204,8 @@ InputOutputRealm::populate_external_variables_from_input(
 //--------------------------------------------------------------------------
 //-------- compute_minimum_distance_to_wall  -------------------------------
 //--------------------------------------------------------------------------
-void InputOutputRealm::compute_wall_distance() {
-    const YAML::Node& wdist = 0;
+void InputOutputRealm::compute_wall_distance(const YAML::Node& wdist) {
+
     stk::mesh::PartVector fluid_parts_;
     stk::mesh::PartVector wall_parts_;
     std::string wall_dist_name_ = "NULL";
