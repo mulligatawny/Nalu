5d04c7a8d7359ba603ac9042c06cf86def5eaac9
mulligatawny <mail.markbenjamin@gmail.com>
2020-08-29 12:11:36 -0700

    .

--- src/InputOutputRealm.C
+++ src/InputOutputRealm.C
@@ -234,8 +234,7 @@ void InputOutputRealm::compute_wall_distance() {
     //}
 
     stk::mesh::Part* fluid_parts_ = meta_data.get_part("fluid-HEX");
-    stk::mesh::Part* wall_parts_[0] = meta_data.get_part("surface1");
-    stk::mesh::Part* wall_parts_[1] = meta_data.get_part("surface2");
+    stk::mesh::Part* wall_parts_ = meta_data.get_part("surface1");
 
     const unsigned nDim = meta_data.spatial_dimension();
 
